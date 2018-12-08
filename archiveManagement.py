69609a6e2757c64fd309aafbb4c15e43d63625e7
Gabriel <090192@gmail.com>
2018-12-08 15:57:04 -0200

    Create archiveManagement.py

---
+++ archiveManagement.py
@@ -0,0 +1,8 @@
+import os.path
+
+#Checking if exist a directory
+if os.path.isdir(dir):
+
+#Creating a directory in a specific structure
+os.mkdir(dir)
+#NOTE - Theres a difference between windows and linux structures
